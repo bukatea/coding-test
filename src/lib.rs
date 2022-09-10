e0d390e309ca4a932d5f199b05669579c995ebc1
Jonathan Lee <bukatea@gmail.com>
2022-09-10 18:20:52 -0400

    implement

---
+++ src/lib.rs
@@ -0,0 +1,236 @@
+//! # Accounts
+//!
+//! `accounts` is a library for describing and managing user accounts.
+
+use std::{collections::HashMap, fmt};
+
+use rust_decimal::Decimal;
+
+/// Client's account
+pub struct Account {
+    /// Client's ID
+    id: u16,
+    /// Record of deposits as (amount, disputed)
+    deposits: HashMap<u32, (Decimal, bool)>,
+    /// Available balance
+    available: Decimal,
+    /// Held balance
+    held: Decimal,
+    /// Locked status
+    locked: bool,
+}
+
+impl Account {
+    /// Create a new empty account
+    pub fn new(id: u16) -> Self {
+        Self {
+            id,
+            deposits: HashMap::new(),
+            available: Decimal::ZERO,
+            held: Decimal::ZERO,
+            locked: false,
+        }
+    }
+
+    /// Deposit funds into the account
+    pub fn deposit(&mut self, txid: u32, amount: Decimal) {
+        // if negative amount, ignore
+        if amount.is_sign_negative() {
+            return;
+        }
+        // record deposit
+        self.deposits.insert(txid, (amount, false));
+        self.available += amount;
+    }
+
+    /// Withdraw funds from the account
+    pub fn withdraw(&mut self, amount: Decimal) {
+        // if locked, disallow withdrawals
+        if self.locked {
+            return;
+        }
+        // if negative amount, ignore
+        if amount.is_sign_negative() {
+            return;
+        }
+        let new_balance = self.available - amount;
+        // if insufficient funds, ignore
+        if new_balance.is_sign_negative() {
+            return;
+        }
+        self.available = new_balance;
+    }
+
+    /// Dispute a transaction
+    pub fn dispute(&mut self, txid: u32) {
+        // only deposits can be disputed
+        if let Some((amount, disputed)) = self.deposits.get_mut(&txid) {
+            // if already disputed, ignore
+            if !*disputed {
+                // hold funds
+                *disputed = true;
+                self.available -= *amount;
+                self.held += *amount;
+            }
+        }
+    }
+
+    /// Resolve a transaction
+    pub fn resolve(&mut self, txid: u32) {
+        // only disputed deposits can be resolved
+        if let Some((amount, disputed)) = self.deposits.get_mut(&txid) {
+            // if not disputed, ignore
+            if *disputed {
+                // release funds
+                *disputed = false;
+                self.available += *amount;
+                self.held -= *amount;
+            }
+        }
+    }
+
+    /// Chargeback a transaction
+    pub fn chargeback(&mut self, txid: u32) {
+        // only disputed deposits can be chargebacked
+        if let Some((amount, disputed)) = self.deposits.get_mut(&txid) {
+            // if not disputed, ignore
+            if *disputed {
+                // reverse transaction and lock account
+                *disputed = false;
+                self.held -= *amount;
+                self.locked = true;
+            }
+        }
+    }
+}
+
+impl fmt::Display for Account {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{},{:.4},{:.4},{:.4},{}",
+            self.id,
+            self.available,
+            self.held,
+            self.available + self.held,
+            self.locked
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_decimal_macros::dec;
+
+    #[test]
+    fn deposit_positive_works() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn deposit_negative_ignored() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(-1.00));
+        assert_eq!(account.available, dec!(0));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn withdraw_positive_works() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.withdraw(dec!(0.50));
+        assert_eq!(account.available, dec!(0.50));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn withdraw_negative_ignored() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.withdraw(dec!(-0.50));
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn withdraw_from_locked_ignored() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.locked = true;
+        account.withdraw(dec!(0.50));
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn withdraw_insufficient_funds_ignored() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.withdraw(dec!(1.50));
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn dispute_deposit_works() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.dispute(1);
+        assert_eq!(account.available, dec!(0));
+        assert_eq!(account.held, dec!(1.00));
+    }
+
+    #[test]
+    fn dispute_nondeposit_ignores() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.dispute(2);
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn resolve_deposit_works() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.dispute(1);
+        account.resolve(1);
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn resolve_undisputed_ignores() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.resolve(1);
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+    }
+
+    #[test]
+    fn chargeback_deposit_works() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.dispute(1);
+        account.chargeback(1);
+        assert_eq!(account.available, dec!(0));
+        assert_eq!(account.held, dec!(0));
+        assert_eq!(account.locked, true);
+    }
+
+    #[test]
+    fn chargeback_undisputed_ignores() {
+        let mut account = Account::new(1);
+        account.deposit(1, dec!(1.00));
+        account.chargeback(1);
+        assert_eq!(account.available, dec!(1.00));
+        assert_eq!(account.held, dec!(0));
+        assert_eq!(account.locked, false);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,103 @@
-fn main() {
-    println!("Hello, world!");
+use std::{path::PathBuf, sync::Arc};
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use csv_async::AsyncDeserializer;
+use dashmap::DashMap;
+use futures::TryStreamExt;
+use rust_decimal::prelude::*;
+use serde::Deserialize;
+use tokio::fs::File;
+
+use coding_test::Account;
+
+/// A transaction type represented by the CSV field `type`
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum CsvTransactionType {
+    Deposit,
+    Withdrawal,
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+/// A transaction represented by a CSV row
+#[derive(Deserialize)]
+struct CsvTransaction {
+    #[serde(rename = "type")]
+    tx_type: CsvTransactionType,
+    client: u16,
+    #[serde(rename = "tx")]
+    txid: u32,
+    amount: Option<Decimal>,
+}
+
+/// Transaction payments engine
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Transactions filename
+    #[clap(value_parser, value_name = "TRANSACTIONS_FILE", value_hint = clap::ValueHint::FilePath)]
+    transactions_filename: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    // stream transactions file
+    let transactions_file = File::open(&args.transactions_filename)
+        .await
+        .with_context(|| {
+            format!(
+                "Failed to read transactions from {}",
+                args.transactions_filename.display()
+            )
+        })?;
+    // create async csv deserializer
+    let mut reader = AsyncDeserializer::from_reader(transactions_file);
+    let records = reader.deserialize::<CsvTransaction>();
+
+    let client_accounts = Arc::new(DashMap::new());
+
+    // process transactions
+    let fut = records.try_for_each_concurrent(None, |transaction| {
+        let client_accounts = client_accounts.clone();
+        async move {
+            // insert client account if it doesn't already exist
+            let mut account = client_accounts
+                .entry(transaction.client)
+                .or_insert(Account::new(transaction.client));
+            // process transaction
+            use CsvTransactionType::*;
+            match transaction.tx_type {
+                Deposit => account.deposit(
+                    transaction.txid,
+                    transaction
+                        .amount
+                        .expect("amount is required for deposit transactions"),
+                ),
+                Withdrawal => account.withdraw(
+                    transaction
+                        .amount
+                        .expect("amount is required for withdrawal transactions"),
+                ),
+                Dispute => account.dispute(transaction.txid),
+                Resolve => account.resolve(transaction.txid),
+                Chargeback => account.chargeback(transaction.txid),
+            };
+
+            Ok(())
+        }
+    });
+    fut.await?;
+
+    // print client accounts
+    println!("client,available,held,total,locked");
+    for account in client_accounts.iter() {
+        println!("{}", *account);
+    }
+
+    Ok(())
 }
